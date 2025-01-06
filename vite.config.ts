8e1a1a0d0d7fe7100ad74b97086b66493860f5b5
aviaviavi <avipress@gmail.com>
2025-01-06 09:35:54 -0800

    GH pages fix

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/customer-analysis/', // Replace with your repo name
 })
